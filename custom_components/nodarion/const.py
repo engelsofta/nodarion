4da09c5ff25428608ccd65658fec8b75484bbf2f
Engelsoft <engelsofta@users.noreply.github.com>
2026-08-09 11:44:33 +0200

    Prepare Nodarion 1.18.0 release

--- custom_components/nodarion/const.py
+++ custom_components/nodarion/const.py
@@ -5,7 +5,7 @@ PLATFORMS = ["binary_sensor"]
 PANEL_URL = "nodarion"
 PANEL_TITLE = "Engelsoft Nodarion"
 INTEGRATION_VERSION = "1.17.9"
-FRONTEND_VERSION = "1.25.0"
+FRONTEND_VERSION = "1.26.0"
 
 CONF_FRITZ_ENABLED = "fritz_enabled"
 CONF_FRITZ_HOST = "fritz_host"
