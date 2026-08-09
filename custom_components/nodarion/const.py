b672ebaa00110115a9a7631d95f54bbccd40260a
Engelsoft <engelsofta@users.noreply.github.com>
2026-08-09 11:48:54 +0200

    Fix integration version for 1.18.0

--- custom_components/nodarion/const.py
+++ custom_components/nodarion/const.py
@@ -4,7 +4,7 @@ DOMAIN = "nodarion"
 PLATFORMS = ["binary_sensor"]
 PANEL_URL = "nodarion"
 PANEL_TITLE = "Engelsoft Nodarion"
-INTEGRATION_VERSION = "1.17.9"
+INTEGRATION_VERSION = "1.18.0"
 FRONTEND_VERSION = "1.26.0"
 
 CONF_FRITZ_ENABLED = "fritz_enabled"
